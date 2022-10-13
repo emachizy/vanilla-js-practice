10186ad4f71782d8a77ccd4ffdae5531dad1e5fb
emachizy <emachi2011@gmail.com>
2022-10-13 14:32:00 -0700

    question project bug fix

--- questions/app.js
+++ questions/app.js
@@ -1,11 +1,26 @@
 //using selectors inside the element
 // traversing the dom
 
-const btns = document.querySelectorAll('.question-btn');
 
-btns.forEach(function(btn){
-    btn.addEventListener('click',function(e){
-        const question = e.currentTarget.parentElement.parentElement;
+const questions = document.querySelectorAll('.question');
+
+questions.forEach((question)=>{
+    const btn = question.querySelector('.question-btn');
+    btn.addEventListener('click',()=>{
+        questions.forEach((item)=>{
+            if(item !== question){
+                item.classList.remove('show-text');
+            }
+        });
         question.classList.toggle('show-text');
-    })
-})
+    });
+});
+
+// const btns = document.querySelectorAll('.question-btn');
+
+// btns.forEach(function(btn){
+//     btn.addEventListener('click',function(e){
+//         const question = e.currentTarget.parentElement.parentElement;
+//         question.classList.toggle('show-text');
+//     })
+// })
